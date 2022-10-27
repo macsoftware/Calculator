5ee03e5b0f7f7344a0066d12932f12fe3c0d1c07
macsoftware <87543117+macsoftware@users.noreply.github.com>
2022-10-27 21:30:18 +0100

    Finishing the UI framework.

--- app/src/main/java/uk/co/macsoftware/calculator/MainActivity.kt
+++ app/src/main/java/uk/co/macsoftware/calculator/MainActivity.kt
@@ -4,17 +4,20 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
-import android.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
 
 
 
+    private fun init(){
 
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        init()
     }
 
 
